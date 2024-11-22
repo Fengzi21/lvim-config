89aefc79e179622b9b343c944926c9d3d0e4f43d
Fengzi21 <iwtbags@163.com>
2024-11-22 11:23:17 +0800

    add asm formatter

--- after/ftplugin/asm.lua
+++ after/ftplugin/asm.lua
@@ -1,1 +1,10 @@
 require("lvim.lsp.manager").setup("asm_lsp")
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+
+formatters.setup({
+  {
+    name = "asmfmt",
+    filetypes = { "s", "asm" }
+  },
+})
